34436cf9d7adc5226194dff7760c19c51bebfdb8
Mac Malone <tydeu@hatpress.net>
2021-04-06 11:45:46 -0400

    Refactored `existsElim` and other code cleanup

--- Gaea/Logic/Basic/Rules.lean
+++ Gaea/Logic/Basic/Rules.lean
@@ -13,13 +13,13 @@ class ForallIntro (L : Logic P) (Fa : LForall P T) :=
   forallIntro : (f : T -> P) -> ((a : T) -> (L |- f a)) -> (L |- lForall f)
 
 def forallIntro {L : Logic P} [Fa : LForall P T]  
-  [K : ForallIntro L Fa] {f : T -> P} := K.forallIntro f
+  [K : ForallIntro L Fa] {f} := K.forallIntro f
 
 class ForallElim (L : Logic P) (Fa : LForall P T) := 
   forallElim : (f : T -> P) -> (L |- lForall f) -> ((a : T) -> (L |- f a))  
 
 def forallElim {L : Logic P} [Fa : LForall P T]  
-  [K : ForallElim L Fa] {f : T -> P} := K.forallElim f
+  [K : ForallElim L Fa] {f} := K.forallElim f
 
 --------------------------------------------------------------------------------
 -- Exists
@@ -29,34 +29,34 @@ class ExistsIntro (L : Logic P) (X : LExists P T) :=
   existsIntro : (f : T -> P) -> (a : T) -> (L |- f a) -> (L |- lExists f)
 
 def existsIntro {L : Logic P} [X : LExists P T]  
-  [K : ExistsIntro L X] {f : T -> P} := K.existsIntro f
+  [K : ExistsIntro L X] {f} := K.existsIntro f
 
 class ExistsElim (L : Logic P) (X : LExists P T) := 
-  existsElim : (f : T -> P) -> (p : P) -> (L |- lExists f) -> 
-    ((a : T) -> (L |- f a) -> (L |- p)) -> (L |- p)
+  existsElim : (r : Sort w) -> (f : T -> P) ->  (L |- lExists f) -> 
+    ((a : T) -> (L |- f a) -> r) -> r
 
 def existsElim {L : Logic P} [X : LExists P T]  
-  [K : ExistsElim L X] {f : T -> P} {p : P} := K.existsElim f p
+  [K : ExistsElim L X] {r f} := K.existsElim r f
 
 --------------------------------------------------------------------------------
 -- If
 --------------------------------------------------------------------------------
 
--- (L |- p -> q) -> (p -> q) 
+-- ((L |- p) -> (L |- q)) -> (L |- p -> q) 
 
 class IfIntro (L : Logic P) (If : LIf P) := 
   ifIntro : (p q : P) -> ((L |- p) -> (L |- q)) -> (L |- p -> q) 
 
 def ifIntro {L : Logic P} [If : LIf P] [K : IfIntro L If] 
-  {p q : P} := K.ifIntro p q
+  {p q} := K.ifIntro p q
 
--- (p -> q) -> (L |- p -> q)
+-- (L |- p -> q) -> ((L |- p) -> (L |- q))
 
 class IfElim (L : Logic P) (If : LIf P) := 
   ifElim : (p q : P) -> (L |- p -> q) -> ((L |- p) -> (L |- q))
 
 def ifElim {L : Logic P} [If : LIf P] [K : IfElim L If] 
-  {p q : P} := K.ifElim p q
+  {p q} := K.ifElim p q
 
 --------------------------------------------------------------------------------
 -- Iff
@@ -68,12 +68,12 @@ class IffIntro (L : Logic P) (Iff : LIff P) (If : LIf P) :=
   iffIntro : (p q : P) -> (L |- p -> q) -> (L |- q -> p) -> (L |- p <-> q)
 
 def iffIntro {L : Logic P} [Iff : LIff P] [If : LIf P]
-  [K : IffIntro L Iff If] {p q : P} := K.iffIntro p q
+  [K : IffIntro L Iff If] {p q} := K.iffIntro p q
 
 def iffIntro' {L : Logic P} [Iff : LIff P] [If : LIf P] 
-  [K : IffIntro L Iff If] [K' : IfIntro L If] {p q : P} 
+  [K : IffIntro L Iff If] [IfI : IfIntro L If] {p q} 
   : ((L |- p) -> (L |- q)) -> ((L |- q) -> (L |- p)) -> (L |- p <-> q)
-  := fun pq qp => K.iffIntro p q (K'.ifIntro p q pq) (K'.ifIntro q p qp)
+  := fun pq qp => K.iffIntro p q (IfI.ifIntro p q pq) (IfI.ifIntro q p qp)
 
 -- (L |- p <-> q) -> (L |- p -> q)
 
@@ -81,10 +81,10 @@ class IffForw (L : Logic P) (Iff : LIff P) (If : LIf P) :=
   iffForw : (p q : P) -> (L |- p <-> q) -> (L |- p -> q)
 
 def iffForw {L : Logic P} [Iff : LIff P] [If : LIf P]
-  [K : IffForw L Iff If] {p q : P} := K.iffForw p q
+  [K : IffForw L Iff If] {p q} := K.iffForw p q
 
 def iffForw' {L : Logic P} [Iff : LIff P] [If : LIf P] 
-  [K : IffForw L Iff If] [K' : IfElim L If] {p q : P} 
+  [K : IffForw L Iff If] [K' : IfElim L If] {p q} 
   : (L |- p <-> q) -> ((L |- p) -> (L |- q))
   := fun pIff => K'.ifElim p q (K.iffForw p q pIff)
 
@@ -94,12 +94,12 @@ class IffBack (L : Logic P) (Iff : LIff P) (If : LIf P) :=
   iffBack : (p q : P) -> (L |- p <-> q) -> (L |- q -> p)
 
 def iffBack {L : Logic P} [Iff : LIff P] [If : LIf P]
-  [K : IffBack L Iff If] {p q : P} := K.iffBack p q
+  [K : IffBack L Iff If] {p q} := K.iffBack p q
 
 def iffBack' {L : Logic P} [Iff : LIff P] [If : LIf P] 
-  [K : IffBack L Iff If] [K' : IfElim L If] {p q : P} 
+  [K : IffBack L Iff If] [IfE : IfElim L If] {p q} 
   : (L |- p <-> q) -> ((L |- q) -> (L |- p))
-  := fun pIff => K'.ifElim q p (K.iffBack p q pIff)
+  := fun pIff => IfE.ifElim q p (K.iffBack p q pIff)
 
 --------------------------------------------------------------------------------
 -- Conjuction
@@ -111,7 +111,7 @@ class ConjIntro (L : Logic P) (Cj : Conj P) :=
   conjIntro : (p q : P) -> (L |- p) -> (L |- q) -> (L |- p /\ q) 
 
 def conjIntro {L : Logic P} [Cj : Conj P] 
-  [K : ConjIntro L Cj] {p q : P} := K.conjIntro p q
+  [K : ConjIntro L Cj] {p q} := K.conjIntro p q
 
 -- p /\ q -> p
 
@@ -119,7 +119,7 @@ class ConjLeft (L : Logic P) (Cj : Conj P) :=
   conjLeft : (p q : P) -> (L |- p /\ q) -> (L |- p)
 
 def conjLeft {L : Logic P} [Cj : Conj P] 
-  [K : ConjLeft L Cj] {p q : P} := K.conjLeft p q
+  [K : ConjLeft L Cj] {p q} := K.conjLeft p q
 
 -- p /\ q -> q
 
@@ -127,7 +127,7 @@ class ConjRight (L : Logic P) (Cj : Conj P) :=
   conjRight : (p q : P) -> (L |- p /\ q) -> (L |- q)
 
 def conjRight {L : Logic P} [Cj : Conj P] 
-  [K : ConjRight L Cj] {p q : P} := K.conjRight p q
+  [K : ConjRight L Cj] {p q} := K.conjRight p q
 
 -- p -> p /\ p
 
@@ -135,7 +135,7 @@ class ConjTaut (L : Logic P) (Cj : Conj P)  :=
   conjTaut : (p : P) -> (L |- p) -> (L |- p /\ p)
 
 def conjTaut {L : Logic P} [Cj : Conj P] 
-  [K : ConjTaut L Cj] {p : P} := K.conjTaut p
+  [K : ConjTaut L Cj] {p} := K.conjTaut p
 
 instance iConjTautOfIntro {L : Logic P} [Cj : Conj P]
   [K : ConjIntro L Cj] : ConjTaut L Cj := 
@@ -147,7 +147,7 @@ class ConjSimp (L : Logic P) (Cj : Conj P)  :=
   conjSimp : (p : P) -> (L |- p /\ p) -> (L |- p)
 
 def conjSimp {L : Logic P} [Cj : Conj P] 
-  [K : ConjSimp L Cj] {p : P} := K.conjSimp p
+  [K : ConjSimp L Cj] {p} := K.conjSimp p
 
 instance iConjSimpOfLeft {L : Logic P} [Cj : Conj P]
   [K : ConjLeft L Cj] : ConjSimp L Cj := 
@@ -160,11 +160,11 @@ instance iConjSimpOfRight {L : Logic P} [Cj : Conj P]
 -- (p /\ q -> a) -> (p -> q -> a)
 
 class ConjCurry (L : Logic P) (Cj : Conj P) :=
-  conjCurry : (a : Sort w) -> (p q : P) -> 
-    ((L |- p /\ q) -> a) -> ((L |- p) -> (L |- q) -> a)
+  conjCurry : (r : Sort w) -> (p q : P) -> 
+    ((L |- p /\ q) -> r) -> ((L |- p) -> (L |- q) -> r)
 
 def conjCurry {L : Logic P} [Cj : Conj P] 
-  [K : ConjCurry L Cj] {a : Sort w} {p q : P} := K.conjCurry a p q
+  [K : ConjCurry L Cj] {r p q} := K.conjCurry r p q
 
 instance iConjCurryOfIntro {L : Logic P} [Cj : Conj P]
   [CjI : ConjIntro L Cj] : ConjCurry L Cj := 
@@ -173,11 +173,11 @@ instance iConjCurryOfIntro {L : Logic P} [Cj : Conj P]
 -- (p -> q -> a) -> (p /\ q -> a)
 
 class ConjUncurry (L : Logic P) (Cj : Conj P) :=
-  conjUncurry : (a : Sort w) -> (p q : P) -> 
-    ((L |- p) -> (L |- q) -> a) -> ((L |- p /\ q) -> a)
+  conjUncurry : (r : Sort w) -> (p q : P) -> 
+    ((L |- p) -> (L |- q) -> r) -> ((L |- p /\ q) -> r)
 
 def conjUncurry {L : Logic P} [Cj : Conj P] 
-  [K : ConjUncurry L Cj] {a : Sort w} {p q : P} := K.conjUncurry a p q
+  [K : ConjUncurry L Cj] {r p q} := K.conjUncurry r p q
 
 instance iConjUncurryOfLeftRight {L : Logic P} [Cj : Conj P]
   [CjL : ConjLeft L Cj] [CjR : ConjRight L Cj] : ConjUncurry L Cj := 
@@ -189,7 +189,7 @@ class ConjOfProd (L : Logic P) (Cj : Conj P) :=
   conjOfProd : (p q : P) -> Prod (L |- p) (L |- q) -> (L |- p /\ q)
 
 def conjOfProd {L : Logic P} [Cj : Conj P] 
-  [K : ConjOfProd L Cj] {p q : P} := K.conjOfProd p q
+  [K : ConjOfProd L Cj] {p q} := K.conjOfProd p q
 
 instance iConjIntroOfProd {L : Logic P} [Cj : Conj P] 
   [K : ConjOfProd L Cj] : ConjIntro L Cj := 
@@ -205,7 +205,7 @@ class ConjOfPProd (L : Logic P) (Cj : Conj P) :=
   conjOfPProd : (p q : P) -> PProd (L |- p) (L |- q) -> (L |- p /\ q)
 
 def conjOfPProd {L : Logic P} [Cj : Conj P] 
-  [K : ConjOfPProd L Cj] {p q : P} := K.conjOfPProd p q
+  [K : ConjOfPProd L Cj] {p q} := K.conjOfPProd p q
 
 instance iConjIntroOfPProd {L : Logic P} [Cj : Conj P] 
   [K : ConjOfPProd L Cj] : ConjIntro L Cj := 
@@ -225,7 +225,7 @@ class ConjOfAnd (L : Logic P) (Cj : Conj P) :=
   conjOfAnd : (p q : P) -> (L |- p) /\ (L |- q) -> (L |- p /\ q)
 
 def conjOfAnd {L : Logic P} [Cj : Conj P] 
-  [K : ConjOfAnd L Cj] {p q : P} := K.conjOfAnd p q
+  [K : ConjOfAnd L Cj] {p q} := K.conjOfAnd p q
 
 instance iConjIntroOfAnd {L : Logic P} [Cj : Conj P] 
   [K : ConjOfAnd L Cj] : ConjIntro L Cj := 
@@ -241,7 +241,7 @@ class ConjAsProd (L : Logic P) (Cj : Conj P) :=
   conjAsProd : (p q : P) -> (L |- p /\ q) -> Prod (L |- p) (L |- q)
 
 def conjAsProd {L : Logic P} [Cj : Conj P] 
-  [K : ConjAsProd L Cj] {p q : P} := K.conjAsProd p q
+  [K : ConjAsProd L Cj] {p q} := K.conjAsProd p q
 
 instance iConjAsProdOfLeftRight {L : Logic P} [Cj : Conj P]
   [CjL : ConjLeft L Cj] [CjR : ConjRight L Cj] : ConjAsProd L Cj := 
@@ -261,7 +261,7 @@ class ConjAsPProd (L : Logic P) (Cj : Conj P) :=
   conjAsPProd : (p q : P) -> (L |- p /\ q) -> PProd (L |- p) (L |- q)
 
 def conjAsPProd {L : Logic P} [Cj : Conj P] 
-  [K : ConjAsPProd L Cj] {p q : P} := K.conjAsPProd p q
+  [K : ConjAsPProd L Cj] {p q} := K.conjAsPProd p q
 
 instance iConjAsPProdOfLeftRight {L : Logic P} [Cj : Conj P]
   [CjL : ConjLeft L Cj] [CjR : ConjRight L Cj] : ConjAsPProd L Cj := 
@@ -285,7 +285,7 @@ class ConjAsAnd (L : Logic P) (Cj : Conj P) :=
   conjAsAnd : (p q : P) -> (L |- p /\ q) -> (L |- p) /\ (L |- q)
 
 def conjAsAnd {L : Logic P} [Cj : Conj P] 
-  [K : ConjAsAnd L Cj] {p q : P} := K.conjAsAnd p q
+  [K : ConjAsAnd L Cj] {p q} := K.conjAsAnd p q
 
 instance iConjAsAndOfLeftRight {L : Logic P} [Cj : Conj P]
   [CjL : ConjLeft L Cj] [CjR : ConjRight L Cj] : ConjAsAnd L Cj := 
@@ -309,7 +309,7 @@ class DisjIntroLeft (L : Logic P) (Dj : Disj P)  :=
   disjIntroLeft : (p q : P) -> (L |- p) -> (L |- p \/ q) 
 
 def disjIntroLeft {L : Logic P} [Dj : Disj P] 
-  [K : DisjIntroLeft L Dj] {p q : P} := K.disjIntroLeft p q
+  [K : DisjIntroLeft L Dj] {p q} := K.disjIntroLeft p q
 
 -- q -> p \/ q
 
@@ -317,16 +317,16 @@ class DisjIntroRight (L : Logic P) (Dj : Disj P)  :=
   disjIntroRight : (p q : P) -> (L |- q) -> (L |- p \/ q) 
 
 def disjIntroRight {L : Logic P} [Dj : Disj P] 
-  [K : DisjIntroRight L Dj] {p q : P} := K.disjIntroRight p q
+  [K : DisjIntroRight L Dj] {p q} := K.disjIntroRight p q
 
 -- p \/ q -> (p -> r) -> (q -> r) -> r
 
 class DisjElim (L : Logic P) (Dj : Disj P) := 
-  disjElim : (a : Sort w) -> (p q : P) -> 
-    (L |- p \/ q) -> ((L |- p) -> a) -> ((L |- q) -> a) -> a
+  disjElim : (r : Sort w) -> (p q : P) -> 
+    (L |- p \/ q) -> ((L |- p) -> r) -> ((L |- q) -> r) -> r
 
 def disjElim {L : Logic P} [Dj : Disj P]
-  [K : DisjElim L Dj] {a : Sort w} {p q : P} := K.disjElim a p q
+  [K : DisjElim L Dj] {r p q} := K.disjElim r p q
 
 -- p -> p \/ p
 
@@ -334,7 +334,7 @@ class DisjTaut (L : Logic P) (Dj : Disj P) :=
   disjTaut : (p : P) -> (L |- p) -> (L |- p \/ p)
 
 def disjTaut {L : Logic P} [Dj : Disj P] 
-  [K : DisjTaut L Dj] {p : P} := K.disjTaut p
+  [K : DisjTaut L Dj] {p} := K.disjTaut p
 
 instance iDisjTautOfIntroLeft {L : Logic P} [Dj : Disj P]
   [K : DisjIntroLeft L Dj] : DisjTaut L Dj := 
@@ -350,7 +350,7 @@ class DisjSimp (L : Logic P) (Dj : Disj P) :=
   disjSimp : (p : P) -> (L |- p \/ p) -> (L |- p)
 
 def disjSimp {L : Logic P} [Dj : Disj P] 
-  [K : DisjSimp L Dj] {p : P} := K.disjSimp p
+  [K : DisjSimp L Dj] {p} := K.disjSimp p
 
 instance iDisjSimpOfElim {L : Logic P} [Dj : Disj P]
   [K : DisjElim L Dj] : DisjSimp L Dj := 
@@ -362,7 +362,7 @@ class DisjElimLeft (L : Logic P) (Dj : Disj P) (Nt : LNot P) :=
   disjElimLeft : (p q : P) -> (L |- p \/ q) -> (L |- ~p) -> (L |- q)
 
 def disjElimLeft {L : Logic P} [Dj : Disj P] [Nt : LNot P] 
-  [K : DisjElimLeft L Dj Nt] {p q : P} := K.disjElimLeft p q
+  [K : DisjElimLeft L Dj Nt] {p q} := K.disjElimLeft p q
 
 -- p \/ q -> ~q -> p
 
@@ -370,7 +370,7 @@ class DisjElimRight (L : Logic P) (Dj : Disj P) (Nt : LNot P) :=
   disjElimRight : (p q : P) -> (L |- p \/ q) -> (L |- ~q) -> (L |- p)
 
 def disjElimRight {L : Logic P} [Dj : Disj P] [Nt : LNot P] 
-  [K : DisjElimRight L Dj Nt] {p q : P} := K.disjElimRight p q
+  [K : DisjElimRight L Dj Nt] {p q} := K.disjElimRight p q
 
 -- Sum p q -> p \/ q 
 
@@ -378,7 +378,7 @@ class DisjOfSum (L : Logic P) (Dj : Disj P) :=
   disjOfSum : (p q : P) -> Sum (L |- p) (L |- q) -> (L |- p \/ q)
 
 def disjOfSum {L : Logic P} [Dj : Disj P] 
-  [K : DisjOfSum L Dj] {p q : P} := K.disjOfSum p q
+  [K : DisjOfSum L Dj] {p q} := K.disjOfSum p q
 
 instance iDisjOfSumOfIntro {L : Logic P} [Dj : Disj P] 
   [DiL : DisjIntroLeft L Dj] [DiR : DisjIntroRight L Dj] : DisjOfSum L Dj := 
@@ -399,7 +399,7 @@ class DisjOfPSum (L : Logic P) (Dj : Disj P) :=
   disjOfPSum : (p q : P) -> PSum (L |- p) (L |- q) -> (L |- p \/ q)
 
 def disjOfPSum {L : Logic P} [Dj : Disj P] 
-  [K : DisjOfPSum L Dj] {p q : P} := K.disjOfPSum p q
+  [K : DisjOfPSum L Dj] {p q} := K.disjOfPSum p q
 
 instance iDisjOfPSumOfIntro {L : Logic P} [Dj : Disj P] 
   [DiL : DisjIntroLeft L Dj] [DiR : DisjIntroRight L Dj] : DisjOfPSum L Dj := 
@@ -420,7 +420,7 @@ class DisjOfOr (L : Logic P) (Dj : Disj P) :=
   disjOfOr : (p q : P) -> (L |- p) \/ (L |- q) -> (L |- p \/ q)
 
 def disjOfOr {L : Logic P} [Dj : Disj P] 
-  [K : DisjOfOr L Dj] {p q : P} := K.disjOfOr p q
+  [K : DisjOfOr L Dj] {p q} := K.disjOfOr p q
 
 instance iDisjOfOrOfIntro {L : Logic P} [Dj : Disj P] 
   [DiL : DisjIntroLeft L Dj] [DiR : DisjIntroRight L Dj] : DisjOfOr L Dj := 
@@ -441,7 +441,7 @@ class DisjAsSum (L : Logic P) (Dj : Disj P) :=
   disjAsSum : (p q : P) -> (L |- p \/ q) -> Sum (L |- p) (L |- q)
 
 def disjAsSum {L : Logic P} [Dj : Disj P] 
-  [K : DisjAsSum L Dj] {p q : P} := K.disjAsSum p q
+  [K : DisjAsSum L Dj] {p q} := K.disjAsSum p q
 
 instance iDisjAsSumOfElim {L : Logic P} [Dj : Disj P] 
   [K : DisjElim L Dj] : DisjAsSum L Dj := 
@@ -458,7 +458,7 @@ class DisjAsPSum (L : Logic P) (Dj : Disj P) :=
   disjAsPSum : (p q : P) -> (L |- p \/ q) -> PSum (L |- p) (L |- q)
 
 def disjAsPSum {L : Logic P} [Dj : Disj P] 
-  [K : DisjAsPSum L Dj] {p q : P} := K.disjAsPSum p q
+  [K : DisjAsPSum L Dj] {p q} := K.disjAsPSum p q
 
 instance iDisjAsPSumOfElim {L : Logic P} [Dj : Disj P] 
   [K : DisjElim L Dj] : DisjAsPSum L Dj := 
@@ -475,7 +475,7 @@ class DisjAsOr (L : Logic P) (Dj : Disj P) :=
   disjAsOr : (p q : P) -> (L |- p \/ q) -> (L |- p) \/ (L |- q)
 
 def disjAsOr {L : Logic P} [Dj : Disj P] 
-  [K : DisjAsOr L Dj] {p q : P} := K.disjAsOr p q
+  [K : DisjAsOr L Dj] {p q} := K.disjAsOr p q
 
 instance iDisjAsOrOfElim {L : Logic P} [Dj : Disj P] 
   [K : DisjElim L Dj] : DisjAsOr L Dj := 
@@ -494,13 +494,13 @@ class NotIntro (L : Logic P) (Nt : LNot P) :=
   notIntro : (p : P) -> ((L |- p) -> False) -> (L |- ~p) 
 
 def notIntro {L : Logic P} [Nt : LNot P]
-  [K : NotIntro L Nt] {p : P} := K.notIntro p
+  [K : NotIntro L Nt] {p} := K.notIntro p
 
 class NotElim (L : Logic P) (Nt : LNot P) := 
   notElim : (p : P) -> (L |- ~p) -> ((L |- p) -> False)
 
 def notElim {L : Logic P} [Nt : LNot P] 
-  [K : NotElim L Nt] {p : P} := K.notElim p
+  [K : NotElim L Nt] {p} := K.notElim p
 
 --------------------------------------------------------------------------------
 -- True
@@ -520,14 +520,14 @@ def Contradiction (L : Logic P) (Nt : LNot P) :=
   PSigma fun (p : P) => PProd (L |- p) (L |- ~p)
 
 def contradiction {L : Logic P} [Nt : LNot P]
-  {p : P} (Lp : L |- p) (LNp : L |- ~p) : Contradiction L Nt := 
+  {p} (Lp : L |- p) (LNp : L |- ~p) : Contradiction L Nt := 
     PSigma.mk p (PProd.mk Lp LNp)
 
 class ByContradiction (L : Logic P) (Nt : LNot P) :=
   byContradiction : (p : P) -> ((L |- p) -> Contradiction L Nt) -> (L |- ~p)
 
 def byContradiction {L : Logic P} [Nt : LNot P]
-  [K : ByContradiction L Nt] {p : P} := K.byContradiction p
+  [K : ByContradiction L Nt] {p} := K.byContradiction p
 
 --------------------------------------------------------------------------------
 -- Absurdity
@@ -543,13 +543,13 @@ class AdAbsurdium (L : Logic P) (A : Absurdity L) (Nt : LNot P) :=
   adAbsurdium : (p : P) -> ((L |- p) -> absurdity L) -> (L |- ~p)
 
 def adAbsurdium {L : Logic P} [A : Absurdity L] [Nt : LNot P]
-  [K : AdAbsurdium L A Nt] {p : P} := K.adAbsurdium p
+  [K : AdAbsurdium L A Nt] {p} := K.adAbsurdium p
 
 class ExAbsurdium (L : Logic P) (A : Absurdity L) :=
   exAbsurdium : (p : P) -> absurdity L -> (L |- p)
 
 def exAbsurdium {L : Logic P} [A : Absurdity L]
-  [K : ExAbsurdium L A] {p : P} := K.exAbsurdium p
+  [K : ExAbsurdium L A] {p} := K.exAbsurdium p
 
 --------------------------------------------------------------------------------
 -- False
@@ -571,6 +571,6 @@ class ExFalso (L : Logic P) (F : LFalse P) :=
   exFalso : (p : P) -> (L |- false) -> (L |- p)
 
 def exFalso {L : Logic P} [F : LFalse P]
-  [K : ExFalso L F] {p : P} := K.exFalso p
+  [K : ExFalso L F] {p} := K.exFalso p
 
 end Gaea.Logic
